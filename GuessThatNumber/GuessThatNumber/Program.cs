82ee94713b8a080754a6b2f8f4fb07dbd0c468e5
anthonymjones <anthony.mjones@gmail.com>
2014-09-22 09:56:41 -0600

    v2
    Cleaned up

--- GuessThatNumber/GuessThatNumber/Program.cs
+++ GuessThatNumber/GuessThatNumber/Program.cs
@@ -12,7 +12,7 @@ namespace GuessThatNumber
         {
             //Tell the user to guess a number, and call the numberGuess function
             
-            Console.WriteLine("\n\nLet's play a game. Guess what number I'm thinking of between 1 and 100.");
+            Console.WriteLine("\n\n Let's play a game. What number am I thinking of between 1 and 100?");
             NumberGuess();
 
             //keep the window open
@@ -32,7 +32,7 @@ namespace GuessThatNumber
             int randomNum = compNum.Next(1, 101);
 
             //set variable for number of guesses
-            int count = 0;
+            int guessCount = 0;
 
             //set place holder for converting guess string to an int
             int userGuess = 0;
@@ -40,6 +40,7 @@ namespace GuessThatNumber
             //Create a loop that runs until the user guesses the randomNumber
             while (userGuess != randomNum)
             {
+
                 string guess = Console.ReadLine();
 
 
@@ -51,36 +52,79 @@ namespace GuessThatNumber
                 {
                     //Computer tells user to guess higher
                     Console.WriteLine();
-                    Console.WriteLine("Guess higher.");
+                    Console.WriteLine(" Guess higher.");
                     
                     //count a guess
-                    count++;
+                    guessCount++;
                 }
                 //userGuess is too high
                 else if (input > randomNum)
                 {
                     //Computer tells user to guess lower
                     Console.WriteLine();
-                    Console.WriteLine("Guess lower.");
+                    Console.WriteLine(" Guess lower.");
                     
 
                     //count a guess
-                    count++;
+                    guessCount++;
                 }
                 //userGuess is correct
                 else if (input == randomNum)
                 {
                     //Computer congratulates the user
-                    count++;
+                    guessCount++;
                     Console.WriteLine();
-                    Console.WriteLine("You got it! It took you " + count + " guess(es) to guess my number.");
+                    Console.WriteLine(" You got it! It took you " + guessCount + " guess(es) to guess my number.");
+                    Console.WriteLine();
                     break;
                     
 
                 }
             }
-
+            //game over
+            AddHighScore(guessCount);
+            DisplayHighScores();
         }
+        static void AddHighScore(int playerScore)
+        {
+            //get the player name for high scores
+            Console.WriteLine(" Your name:");
+            string playerName = Console.ReadLine();
 
+            //create a gateway to the database
+            AnthonyEntities db = new AnthonyEntities();
+            
+            //create a new highscore object
+            HighScore newHighScore = new HighScore();
+            newHighScore.DateCreated = DateTime.Now;
+            newHighScore.Game = "Guess That Number";
+            newHighScore.Name = playerName;
+            newHighScore.Score = playerScore;
+
+            //add to the database
+            db.HighScores.Add(newHighScore);
+            
+            //save our changes
+            db.SaveChanges();
+        }
+        static void DisplayHighScores()
+        {
+            //clear the console
+            Console.Clear();
+            //Write the High Score Text
+            Console.WriteLine();
+            Console.WriteLine("                      Guess That Number High Scores");
+            Console.WriteLine("                      *****************************");
+
+            //create a new connection to the db
+            AnthonyEntities db = new AnthonyEntities();
+            //get the high score list
+            List<HighScore> highScoreList = db.HighScores.Where(x => x.Game == "Guess That Number").OrderBy(x => x.Score).Take(10).ToList();
+
+            foreach (HighScore highScore in highScoreList)
+            {
+                Console.WriteLine("                       {0}. {1} - {2} on {3}", highScoreList.IndexOf(highScore) + 1, highScore.Name, highScore.Score, highScore.DateCreated.Value.ToShortDateString());
+            }
+        }
     }
 }
